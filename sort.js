e1684a39d89d26c712485ac0ebec2217c3abfbbd
YasuhikoKitamura2 <ykitamura@gaia.eonet.ne.jp>
2026-02-09 16:14:22 +0900

    配列をソートする機能

--- sort.js
+++ sort.js
@@ -1,2 +1,8 @@
+var sortNumber = function(number){
+    return number.sort(function(a, b){return a - b});}
+    
 var number = [19,3];
+
+console.log(number);
+sortNumber(number);
 console.log(number);
